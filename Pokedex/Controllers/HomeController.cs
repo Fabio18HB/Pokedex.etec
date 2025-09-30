2f255b5540cc91324f28f8e476ccd6b74c190aa4
Fabio18HB <fabheriq@gmail.com>
2025-09-30 13:29:37 -0300

    nova pokedex

---
+++ Pokedex/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Data;
+using Pokedex.Models;
+using Pokedex.ViewModels;
+
+namespace Pokedex.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _db;
+
+    public HomeController(
+        ILogger<HomeController> logger, 
+        AppDbContext db
+    )
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        HomeVM home = new() {
+            Tipos = _db.Tipos.ToList(),
+            Pokemons = _db.Pokemons
+                .Include(p => p.Tipos)
+                .ThenInclude(t => t.Tipo)
+                .ToList()
+        };
+        return View(home);
+    }
+
+    public IActionResult Details(int id)
+    {
+        Pokemon pokemon = _db.Pokemons
+                            .Where(p => p.Numero == id)
+                            .Include(p => p.Regiao)
+                            .Include(p => p.Genero)
+                            .Include(p => p.Tipos)
+                            .ThenInclude(t => t.Tipo)   
+                            .SingleOrDefault();
+        DetailVM detail = new()
+        {
+            Atual = pokemon,
+            Anterior = _db.Pokemons
+                .OrderByDescending(p => p.Numero)
+                .FirstOrDefault(p => p.Numero < id),
+            Proximo = _db.Pokemons
+                .OrderBy(p => p.Numero)
+                .FirstOrDefault(p => p.Numero > id)
+        };                          
+        return View(detail);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Pokedex.Models;
+
+[Table("Pokemon")]
+public class Pokemon
+{
+    [Key]
+    [Display(Name = "Número", Prompt="Informe o Número")]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public uint Numero { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe a Região")]
+    public uint RegiaoId { get; set; }
+    [ForeignKey("RegiaoId")]
+    public Regiao Regiao { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o Gênero")]
+    public uint GeneroId { get; set; }
+    [ForeignKey("GeneroId")]
+    public Genero Genero { get; set; }
+
+    [StringLength(30)]
+    [Required(ErrorMessage = "Por favor, informe o nome")]
+    public string Nome { get; set; }
+    [StringLength(1000)]
+    public string Descricao { get; set; }
+
+    [Column(TypeName = "double(5,2)")]
+    [Required(ErrorMessage = "Por favor, informe a altura")]
+
+    public double Altura { get; set; }
+
+    [Column(TypeName = "double(7,3)")]
+    [Required(ErrorMessage = "Por favor, informe o peso")]
+    public double Peso { get; set; }
+
+    [StringLength(200)]
+    public string Imagem { get; set; }
+
+    [StringLength(400)]
+    public string Animacao { get; set; }
+
+    public ICollection<PokemonTipo> Tipos { get; set; }
+}
